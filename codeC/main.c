53fa1aed3c02e4205637535bef62e046a8b31e58
Damif <damifernandes22@gmail.com>
2024-12-15 17:12:00 +0100

    ajout gnuplot

--- codeC/main.c
+++ codeC/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // Pour la fonction access
+#include <unistd.h>
 #include "avl.h"
 #include "io.h"
 
-#define DEBUG 1 // Activez ou désactivez les messages de débogage
+#define DEBUG 1
 
 int main(int argc, char *argv[]) {
     if (DEBUG) {
@@ -17,11 +17,6 @@ int main(int argc, char *argv[]) {
 
     if (argc != 4) {
         fprintf(stderr, "Erreur : Nombre d'arguments incorrect : %d\n", argc);
-        fprintf(stderr, "DEBUG: Arguments fournis : ");
-        for (int i = 0; i < argc; i++) {
-            fprintf(stderr, "'%s' ", argv[i]);
-        }
-        fprintf(stderr, "\n");
         fprintf(stderr, "Usage: %s <fichier_entree.dat> <fichier_sortie.dat> <has_header>\n", argv[0]);
         return EXIT_FAILURE;
     }
@@ -30,45 +25,38 @@ int main(int argc, char *argv[]) {
     const char *fichier_sortie = argv[2];
     int has_header = atoi(argv[3]);
 
+    // Vérification des cas interdits
+    if (strstr(fichier_sortie, "hvb_all") || strstr(fichier_sortie, "hvb_indiv") ||
+        strstr(fichier_sortie, "hva_all") || strstr(fichier_sortie, "hva_indiv")) {
+        fprintf(stderr, "Erreur : Cas interdit détecté dans les options de sortie (%s).\n", fichier_sortie);
+        return EXIT_FAILURE;
+    }
+
     if (DEBUG) {
-        printf("DEBUG: Chemin d'entrée reçu : '%s'\n", fichier_entree);
-        for (size_t i = 0; i < strlen(fichier_entree); i++) {
-            printf("DEBUG: fichier_entree[%zu] = '%c' (ASCII: %d)\n", i, fichier_entree[i], fichier_entree[i]);
-        }
+        printf("DEBUG: Chemin d'entrée : %s\n", fichier_entree);
     }
 
     if (access(fichier_entree, F_OK) != 0) {
         perror("Erreur : Le fichier d'entrée est inaccessible");
-        fprintf(stderr, "DEBUG: Chemin testé : %s\n", fichier_entree);
         return EXIT_FAILURE;
     }
 
-    if (DEBUG) printf("DEBUG: Fichier d'entrée accessible : %s\n", fichier_entree);
-
-    // Charger les données dans l'arbre AVL
     NoeudAVL *arbre = charger_dat_dans_avl(fichier_entree, has_header);
     if (!arbre) {
-        fprintf(stderr, "Erreur : Arbre AVL non généré à partir de %s\n", fichier_entree);
+        fprintf(stderr, "Erreur : Arbre AVL non généré\n");
         return EXIT_FAILURE;
     }
 
-    if (DEBUG) printf("DEBUG: Arbre AVL généré avec succès.\n");
-    afficher_avl(arbre);
-
-    // Calculer la somme des capacités
     long somme = somme_avl(arbre);
     printf("Consommation totale (Capacity) : %ld\n", somme);
 
-    // Générer le fichier de sortie
     if (!generer_fichier_sortie(fichier_sortie, arbre)) {
-        fprintf(stderr, "Erreur : Impossible de générer le fichier de sortie %s\n", fichier_sortie);
+        fprintf(stderr, "Erreur : Impossible de générer le fichier de sortie\n");
         liberer_avl(arbre);
         return EXIT_FAILURE;
     }
 
-    if (DEBUG) printf("DEBUG: Fichier de sortie généré avec succès : %s\n", fichier_sortie);
     liberer_avl(arbre);
-
     printf("Traitement terminé. Résultats enregistrés dans %s\n", fichier_sortie);
     return EXIT_SUCCESS;
 }
